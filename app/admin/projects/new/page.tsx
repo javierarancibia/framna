73e49757eabc541f11d0bd18ac4940afc4c16b4c
javier@hodlgroup.com <javier@hodl.nl>
2025-06-12 23:27:49 +0200

    Refactor NewProjectPage form; enhance input fields and layout for better user experience Remove unused CSS variables and theme definitions from globals.css

--- app/admin/projects/new/page.tsx
+++ app/admin/projects/new/page.tsx
@@ -1,70 +1,105 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 type FormData = {
-    name: string;
-    description: string;
-    image?: string;
+  name: string;
+  description: string;
+  image?: string;
 };
 
 export default function NewProjectPage() {
-    const { register, handleSubmit } = useForm<FormData>();
-    const router = useRouter();
+  const { register, handleSubmit } = useForm<FormData>();
+  const router = useRouter();
 
-    // Funtion to handle form submission to create new project
-    const onSubmit = async (data: FormData) => {
-        try {
-            const url = process.env.NEXT_PUBLIC_MOCKAPI_URL;
-            if (!url) {
-                console.error("Falta NEXT_PUBLIC_MOCKAPI_URL");
-                return;
-            }
-            await axios.post(url, data);
-            router.push('/admin');
-        } catch (error) {
-            console.error('Error creating the project:', error);
-        }
-    };
+  // Funtion to handle form submission to create new project
+  const onSubmit = async (data: FormData) => {
+    try {
+      const url = process.env.NEXT_PUBLIC_MOCKAPI_URL;
+      if (!url) {
+        console.error("Falta NEXT_PUBLIC_MOCKAPI_URL");
+        return;
+      }
+      await axios.post(url, data);
+      router.push("/admin");
+    } catch (error) {
+      console.error("Error creating the project:", error);
+    }
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="mb-6 text-2xl font-bold">New Project</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 lg:px-8">
+            <div className="border-b border-gray-900/10 pb-12">
+                <h2 className="text-base/7 font-semibold text-gray-900">Profile</h2>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 max-w-md">
-            <div>
-                <label className="block text-sm font-medium">Nombre del proyecto</label>
-                <input
-                    {...register('name', { required: true })}
-                    className="mt-1 w-full rounded border p-2"
-                />
+                <div className="mt-10">
+                    <div className="my-5">
+                        <label
+                            htmlFor="name"
+                            className="block text-sm/6 font-medium text-gray-900"
+                        >
+                            Project Name
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input
+                                     {  ...register("name", { required: true}) }
+                                    type="text"
+                                    placeholder="Project Name"
+                                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="my-5">
+                        <label
+                            htmlFor="image"
+                            className="block text-sm/6 font-medium text-gray-900"
+                        >
+                            Image Url (optional)
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input
+                                     {  ...register("image", { required: true}) }
+                                    type="text"
+                                    placeholder="Image URL"
+                                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="my-5">
+                        <label
+                            htmlFor="description"
+                            className="block text-sm/6 font-medium text-gray-900"
+                        >
+                            Description
+                        </label>
+                        <div className="mt-2">
+                            <textarea
+                                rows={3}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                defaultValue={""}
+                                {  ...register("description", { required: true}) }
+                            />
+                        </div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="rounded-md bg-blue-400 w-full px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-300 hover:cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                    Create Project
+                    </button>
+                </div>
             </div>
-
-            <div>
-                <label className="block text-sm font-medium">Descripción</label>
-                <textarea
-                    {...register('description')}
-                    className="mt-1 w-full rounded border p-2"
-                />
-            </div>
-
-            <div>
-                <label className="block text-sm font-medium">URL de imagen (opcional)</label>
-                <input
-                    {...register('image')}
-                    className="mt-1 w-full rounded border p-2"
-                />
-            </div>
-
-            <button
-                type="submit"
-                className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
-            >
-                Create Project
-            </button>
-        </form>
-    </div>
+        </div>
+    </form>
   );
 }
