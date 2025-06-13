853282aa78d8810cde5dc6dfaae9565bf282ce02
javier@hodlgroup.com <javier@hodl.nl>
2025-06-13 11:19:05 +0200

    Setup Jest configuration and add necessary mocks; update package dependencies for testing

---
+++ jest.config.mjs
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+};
+
+export default createJestConfig(customJestConfig);
--- tests/Hero.test.tsx
+++ tests/Hero.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Hero from '@/components/Hero';
 
+// Importing jest-dom for extended matchers
 describe('Hero component', () => {
   it('renders my name and role', () => {
     render(<Hero />);
     expect(screen.getByText(/Javier Arancibia/i)).toBeInTheDocument();
-    expect(screen.getByText(/Front End Developer/i)).toBeInTheDocument();
+    expect(screen.getByText(/Front End Developer & Product Builder/i)).toBeInTheDocument();
   });
 
   it('has an About Me button', () => {
